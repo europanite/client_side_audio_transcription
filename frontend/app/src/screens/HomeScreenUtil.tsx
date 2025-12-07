71d3c82bb7466c2a05c5a5955a75db27ef3a3bc2
europanite <europanite@gmail.com>
2025-12-08 05:04:21 +0900

    fix/link

--- frontend/app/src/screens/HomeScreenUtil.tsx
+++ frontend/app/src/screens/HomeScreenUtil.tsx
@@ -1,5 +1,5 @@
 export const REPO_URL =
-  "https://github.com/europanite/client_side_transcription";
+  "https://github.com/europanite/client_side_audio_transcription";
 
 export const REPO_URL_PAGE =
-  "https://europanite.github.io/client_side_transcription/";
+  "https://europanite.github.io/client_side_audio_transcription/";
