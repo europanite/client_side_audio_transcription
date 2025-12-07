cb944f161330df89c7f9734864c84fdaf3ff4693
europanite <europanite@gmail.com>
2025-12-08 05:15:29 +0900

    fix/config

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/client_side_audio_transcription/",
 
   plugins: [react()],
 
